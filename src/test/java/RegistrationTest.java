7593c4dda30613ef35d03d6f75aab57174157dbb
olgakolchina <olgakolcina861@gmail.com>
2023-11-14 14:33:27 +0200

    testBase

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,59 +1,73 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class RegistrationTest extends TestBase {
+//    WebDriver wd;
+//
+//    @BeforeSuite
+//    public void init() {
+//        wd = new ChromeDriver();
+//        wd.navigate().to("https://telranedu.web.app/home");
+//        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//    }
 
-public class RegistrationTest {
-
-
-    WebDriver wd;
-
-    @BeforeMethod
-    public void init() {
-        wd = new ChromeDriver();
-        wd.navigate().to("https://telranedu.web.app/home");
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
 
     @Test
-    public void registrationPositive(){
+    public void registrationPositive() {
         wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-        int i=(int)(System.currentTimeMillis()/1000)%3600;
+        int i = (int) (System.currentTimeMillis() / 1000) % 3600;
         //fill login form
         // email
         WebElement emailInput = wd.findElement(By.xpath(" //input[1]"));
         emailInput.click();
         emailInput.clear();
-        emailInput.sendKeys("cherry"+ i + "@gmail.com");
+        emailInput.sendKeys("cherry" + i + "@gmail.com");
 //password
         WebElement passInput = wd.findElement(By.xpath("//input[2]"));
         passInput.click();
         passInput.clear();
         passInput.sendKeys("Ch12345$");
 
-// click on button Registration
+       // click on button Registration
         wd.findElement(By.xpath(" //button[2]")).click();
 
         // Assert
 
         //   Assert.assertTrue (wd.findElements(By.xpath("//*[.='Sign Out']")).size() > 0);
-        Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
-
+        //  Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
+        pause(5000);
+        Assert.assertTrue(isElementPresent(By.xpath("//button")));
     }
+
+    @Test
+    public void registrationNegative() {
+        int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+        String email = "cherry" + i + "gmail.com";
+        String password = "Ch12345$";
+        openLoginForm();
+        fillLoginForm(email, password);
+        submitRegistration();
+    }
+    @Test
+    public void registrationNegativePassword() {
+        int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+        String email = "cherry" + i + "@gmail.com";
+        String password = "Ch12345";
+        openLoginForm();
+        fillLoginForm(email, password);
+        submitRegistration();
+    }
+
 
     @AfterMethod
 
-    public void tearDown () {
+    public void tearDown() {
+        //  wd.quit();
 
     }
-
-
 
 
 }
--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -17,21 +17,36 @@ public class Start {
     public void test(){
         wd.findElement(By.tagName("a"));
         wd.findElement(By.cssSelector("a"));
+        wd. findElement (By.xpath("//a"));
 
         wd.findElement(By.id("root"));
         wd.findElement(By.cssSelector("#root"));
+        wd.findElement (By.xpath( "//*[@id='root']"));
 
         wd.findElement(By.className("container"));
         wd.findElement(By.cssSelector(".container"));
+        wd.findElement(By.xpath("//*[@class='container']"));
 
         wd.findElement(By.linkText("HOME"));
         wd.findElement(By.partialLinkText("HO"));
+        wd.findElement (By.xpath ("//a[text()='HOME']"));
+        wd.findElement (By.xpath (  "//a[.='HOME']"));
 
         wd.findElement(By.cssSelector("[href='/login']"));
+        wd. findElement (By.xpath ("//a[@href='/login']"));
+
         wd.findElement (By.cssSelector("[href*='og']"));
+        wd. findElement (By.xpath( "//a[contains(@href,'og')]"));
+
         wd.findElement (By.cssSelector("[href^='/log']"));
+        wd .findElement (By.xpath ( "//a[starts-with(@href,'/log')]"));
         wd.findElement (By.cssSelector("[href$='gin']"));
 
+        //*[@id="__next"]/div[1]/main/form/div[1]/div[1]//input      name
+        //*[@id="__next"]/div[1]/main/form/div[1]/div[2]//input    last name
+        //*[@id="__next"]/div[1]/main/form/div[2]//input            email
+        //*[@id="__next"]//div[3]//input                             password
+         //*[@id="__next"]/div[1]/main/form/button
     }
     @AfterTest
     public void postcondition(){
@@ -39,3 +54,9 @@ public class Start {
 
     }
 }
+// //*[@id='name']
+//*[@id='lastName']
+//*[@id='email']
+//*[@id='password']
+//*[@id='terms-of-use']
+//form/button
