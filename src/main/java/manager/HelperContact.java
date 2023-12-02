010cf814eaa7f2cb0d7d222b692527e7ecfd8096
olga-82 <134201905+olga-82@users.noreply.github.com>
2023-12-02 20:56:39 +0200

    contact

--- src/main/java/manager/HelperContact.java
+++ src/main/java/manager/HelperContact.java
@@ -3,8 +3,15 @@ package manager;
 import model.Contact;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class HelperContact extends HelperBase {
+
+    Logger logger= LoggerFactory.getLogger(HelperContact.class);
     public HelperContact(WebDriver wd) {
         super(wd);
     }
@@ -28,10 +35,66 @@ public class HelperContact extends HelperBase {
     }
 
     public boolean isContactCreated(Contact contact) {
-      String phone= wd.findElement (By.xpath (  "//div[@class='contact-item_card__2SOIM'][last()]//h3")).getText();
-      return phone.equals(contact.getPhone());
+        String phone = wd.findElement(By.xpath("//div[@class='contact-item_card__2SOIM'][last()]//h3")).getText();
+        return phone.equals(contact.getPhone());
 
     }
+    public int countContacts(){
+        return wd.findElements(By.xpath("//div[@class='contact-item_card__2SOIM']")).size();
+    }
+    public int removeOneContact(){
+        int countBefore = countContacts();
+        logger.info("Amount of contacts before is " + countBefore);
+        click(By.xpath("//div[@class='contact-item_card__2SOIM']"));
+        click(By.xpath("//button[.='Remove']"));
+        pause(5000);
+        int countAfter = countContacts();
+        logger.info("Amount of contacts after is " + countAfter);
+        return countAfter - countBefore;
+    }
+
+    public void removeAllContacts(){
+        while (wd.findElements(By.xpath("//div[@class='contact-item_card__2SOIM']")).size() > 0){
+            removeOneContact();
+        }
+    }
+
+    public boolean isNoContacts(){
+        return wd.findElements(By.xpath("//div[@class='contact-item_card__2SOIM']")).size()==0;
+    }
+
+
+
+
+
+
+//
+//    public void setSize() {
+//    List<WebElement> listInputs = wd.findElements(By.xpath("//*[@id='root']/div[2]/div/div/div[1]/div/div"));
+//    System.out.println("length of list  " + listInputs.size());
+//
+//    WebElement parentOfInput =  wd.findElement(By.xpath("//*[@id='root']/div[2]/div/div/div[1]/div/div"));
+//    System.out.println("print attribute name " + parentOfInput.getText().toString());
+//
+//}
+
+
+    public void clickBoxWithContact() {
+        wd.findElement(By.xpath("//div[@class='contact-item_card__2SOIM'][1]")).click();
+
+    }
+    public boolean isBoxPresent(){
+      return isElementPresent(By.xpath("//*[@id='root']/div[2]/div/div/div[2]/div"));
+    }
+
+
+    public void clickButtonRemove() {
+        wd.findElement(By.xpath("//button[.='Remove']")).click();
+
+    }
+    //*[@id="root"]/div[2]/div/div/div[1]/div/div[1]/h2
+
+    //button[.='Remove']
 }
 
 
--- src/test/java/tests/AddNewContactTests.java
+++ src/test/java/tests/AddNewContactTests.java
@@ -3,6 +3,7 @@ package tests;
 import manager.TestNgListeners;
 import model.Contact;
 import model.User;
+import org.openqa.selenium.By;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -23,14 +24,15 @@ public class AddNewContactTests extends TestBase {
         }
 
     }
-    @Test(invocationCount = 5)
-    public void addNewContactPositive(){
+
+    @Test(invocationCount = 1)
+    public void addNewContactPositive() {
         int i = (int) (System.currentTimeMillis() / 1000) % 3600;
-        Contact contact =  Contact.builder()
+        Contact contact = Contact.builder()
                 .name("Sara_" + i)
                 .lastName("Braun")
-                .phone("65840033"+i)
-                .email("cher_"+i+"@gmail.com")
+                .phone("65840033" + i)
+                .email("cher_" + i + "@gmail.com")
                 .address("Tel Aviv")
                 .description("friend")
                 .build();
@@ -40,5 +42,9 @@ public class AddNewContactTests extends TestBase {
         app.getContact().submitContactForm();
         app.getUser().pause(1000);
         Assert.assertTrue(app.getContact().isContactCreated(contact));
+
+
     }
+
+
 }
